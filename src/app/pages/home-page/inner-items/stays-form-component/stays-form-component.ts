d9606d5fc805db907e98326bcd5983ab085dca14
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2025-08-24 23:23:24 +0530

    Add some features to search bar

--- src/app/pages/home-page/inner-items/stays-form-component/stays-form-component.ts
+++ src/app/pages/home-page/inner-items/stays-form-component/stays-form-component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stays-form-component',
   standalone: true,
-  imports: [],  // add other modules if needed
+  imports: [
+    MatTabsModule,   // ✅ this gives access to <mat-tab-group> and <mat-tab>
+    MatIconModule,],  // add other modules if needed
   templateUrl: './stays-form-component.html',
   styleUrls: ['./stays-form-component.scss']  // ✅ fixed
 })
