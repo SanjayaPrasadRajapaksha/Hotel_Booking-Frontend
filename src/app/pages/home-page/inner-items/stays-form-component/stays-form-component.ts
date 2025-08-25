8a42f423d9a996c839a023b484b81cc7c447dd4c
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2025-08-25 15:57:05 +0530

    Implement booking search bar

--- src/app/pages/home-page/inner-items/stays-form-component/stays-form-component.ts
+++ src/app/pages/home-page/inner-items/stays-form-component/stays-form-component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-stays-form-component',
   standalone: true,
   imports: [
-    MatTabsModule,   // ✅ this gives access to <mat-tab-group> and <mat-tab>
-    MatIconModule,],  // add other modules if needed
+    MatTabsModule,        // for <mat-tab-group> and <mat-tab>
+    MatIconModule,        // for material icons
+    MatFormFieldModule,   // for <mat-form-field>
+    MatDatepickerModule,  // for datepicker & date range
+    MatInputModule        // required for <input matInput>
+  ],
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './stays-form-component.html',
-  styleUrls: ['./stays-form-component.scss']  // ✅ fixed
+  styleUrls: ['./stays-form-component.scss']
 })
 export class StaysFormComponent { }
