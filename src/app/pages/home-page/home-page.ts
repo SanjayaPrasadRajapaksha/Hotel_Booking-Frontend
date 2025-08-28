2669360baa8fffae47a28a225b15a9824e0aca0c
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2025-08-28 22:38:52 +0530

    Add offer section

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MainHeader} from './inner-items/main-header/main-header';
 import {OptionsComponent} from './inner-items/options-component/options-component';
+import {OffersComponent} from './inner-items/offers-component/offers-component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,           // 👈 if you want standalone
   imports: [
     MainHeader,
-    OptionsComponent
+    OptionsComponent,
+    OffersComponent
   ],                // 👈 only if using standalone + importing other modules
   templateUrl: './home-page.html',
   styleUrls: ['./home-page.scss']
---
+++ src/app/pages/home-page/inner-items/offers-component/offers-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './offers-component.html',
+  styleUrls: ['./offers-component.scss']
+})
+export class OffersComponent {}
