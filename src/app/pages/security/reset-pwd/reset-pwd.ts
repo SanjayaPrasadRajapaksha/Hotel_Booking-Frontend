5d238dfb63cf8ef0762c30ecfb9785d5a94df457
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2025-08-31 21:48:17 +0530

    Create reset password form

---
+++ src/app/pages/security/reset-pwd/reset-pwd.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-reset-pwd',
+  standalone: true, 
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    RouterModule
+  ],
+  templateUrl: './reset-pwd.html',
+  styleUrl: './reset-pwd.scss'
+})
+export class ResetPwd {
+
+}
