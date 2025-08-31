b73482f1b194f9bde133984b0974c18f1dc15ac4
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2025-08-31 11:13:20 +0530

    Create login UI

--- src/app/pages/security/login/login.ts
+++ src/app/pages/security/login/login.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true, 
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
-
+showStatus: boolean = false;
 }
--- src/app/pages/security/security-context/security-context.ts
+++ src/app/pages/security/security-context/security-context.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-security-context',
-  standalone: true, 
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './security-context.html',
   styleUrl: './security-context.scss'
 })
